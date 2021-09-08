bf8671a56bbb90a2ffe15fd43d3cc3b689f6e2a5
jaehyun <jaehyuna11@gmail.com>
2021-09-08 00:26:37 -0700

    File open and write practice

---
+++ Day_24/launcher.py
@@ -0,0 +1,14 @@
+# with will manage and close the file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#
+# # mode "a" means append
+# # mode "w" means write(overwrite)
+# with open("new_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# if my_file is located in the desktop
+# with open("../../my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
