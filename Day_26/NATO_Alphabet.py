5957cd33d7b7d26ffc4f11a714b5cba4378006f8
jaehyun <jaehyuna11@gmail.com>
2021-09-08 23:04:01 -0700

    Tkinter first program (converter)

--- Day_26/NATO_Alphabet.py
+++ Day_26/NATO_Alphabet.py
@@ -8,12 +8,16 @@ print(data.to_dict())
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
+trial = 0
 program_on = True
 while program_on:
     # TODO2 : Create a list of the phonetic code words from a word that the user inputs.
     word = input("Enter a word: ").upper()
     output_list = [phonetic_dict[letter] for letter in word]
+    trial += 1
     print(output_list)
+    if trial == 2:
+        program_on = False
 
 
 
